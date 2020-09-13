1e0b2e6045dc2d9f86b764a3f89cffa0e885537e
perth pp <kekskcfm@gmail.com>
2020-09-13 16:42:34 +0700

    edit

---
+++ week5/Source2.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int number, i, x, n = 1;
+    scanf("%d", &number);
+    n = number - 1;
+    for (x = 1; x <= number; x++)
+    {
+        for (i = 1; i <= n; i++)
+            printf(" ");
+        n--;
+        for (i = 1; i <= 2 * x - 1; i++)
+            printf("*");
+        printf("\n");
+    }
+    n = 1;
+    for (x = 1; x <= number - 1; x++)
+    {
+        for (i = 1; i <= n; i++)
+            printf(" ");
+        n++;
+        for (i = 1; i <= 2 * (number - x) - 1; i++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
