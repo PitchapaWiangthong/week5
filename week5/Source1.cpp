b338512574359d9a9106fdace2023158aae6f07b
perth pp <kekskcfm@gmail.com>
2020-09-13 16:19:13 +0700

    edit var

--- week5/Source1.cpp
+++ week5/Source1.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h> 
 int main() {
-    int x;
-    scanf_s("%d", &x);
+    unsigned int x;
+    scanf_s("%ud", &x);
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < x; j++)
